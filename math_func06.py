94782a31d10bd7ce34122488b2b93d7f60464c5c
Muhammad Ali <kamoliddeenov@gmail.com>
2021-12-24 10:35:21 +0500

    6-misol

--- math_func06.py
+++ math_func06.py
@@ -1,7 +1,7 @@
-# Create the main function.
+from math import tan, degrees
+def main(a,b):
+    c = a * tan(b)
+    d = degrees(c)
+    return round(d,2)
 
-# Multiplies one number by the tangent of another number.
-
-# Convert the number from radians to degrees.
-
-# Round the result to 2 decimal places.
+print(main(18,8))
