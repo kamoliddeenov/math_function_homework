39da346723e20f0a5e6b01da197d4c0b55e4cf15
Muhammad Ali <kamoliddeenov@gmail.com>
2021-12-24 13:40:18 +0500

    4-misol

--- math_func04.py
+++ math_func04.py
@@ -1,6 +1,5 @@
-from math import sin, degrees, pi
+from math import sin, degrees, pi, radians
 def  main(a,b):
-    b = pi/2
-    return round(degrees(a*sin(b)), 2)
+    return round(degrees(a*radians(sin(b))), 2)
 
-print(main(4,90))
+print(main(60,90))
